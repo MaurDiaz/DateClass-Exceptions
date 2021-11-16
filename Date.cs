f587330eec30988dd6a200c15c393a6d379a8fa1
Mauricio Diaz <mrcdz@pm.me>
2021-11-16 10:35:52 -0700

    Add LeapYear method

--- Date.cs
+++ Date.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 namespace ActivityE1
 {
     class Date
     {
+        
+        private Dictionary<int, int> daysInAMonth = new Dictionary<int, int>(){ 
+            {1,31},{2,28},{3,31},{4,30},{5,31},{6,30},{7,31},{8,31},{9,30},{10,31},{11,30},{12,31}    
+        };
         public int Day {get; set;}
         public int Month {get; set;}
         public int Year {get; set;}
@@ -12,7 +17,31 @@ namespace ActivityE1
         {
 
         }
-
+        public int DaysOfTheMonth(int month)
+        {
+            return daysInAMonth[month];
+        }
+        public bool LeapYear(int year)
+        {
+            bool isLeapYear = false;
+            if(year % 4 == 0)
+            {
+                if(year % 100 == 0)
+                {
+                    if(year % 400 == 0)
+                    {
+                        isLeapYear = true;
+                        return isLeapYear;
+                    }
+                }
+                else
+                {
+                    isLeapYear = true;
+                    return isLeapYear;
+                }
+            }
+            return isLeapYear;
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,65 @@ namespace ActivityE1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool hasRightFormat = false;
+            int year, month, day;
+            Date date  = new Date();
+            
+            do{
+                try
+                {
+                    Console.WriteLine("Enter a year:");
+                    year = int.Parse(Console.ReadLine());
+                    if(year < 0)
+                    {
+                        throw new InvalidDateException(year);
+                    }
+                    date.Year = year;
+                    hasRightFormat = true;
+                }
+                catch(InvalidDateException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Year must be greater than zero. Please try again.");
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                } 
+            }while(!hasRightFormat);
+            
+            hasRightFormat = false;
+            Console.WriteLine(date.LeapYear(date.Year));
+
+            // do{
+            //     try
+            //     {
+            //         Console.WriteLine("Enter a month:");
+            //         month = int.Parse(Console.ReadLine());
+            //         if(month < 1 || month > 12 )
+            //         {
+            //             throw new InvalidDateException(month);
+            //         }
+            //     }
+            //     catch(InvalidDateException e)
+            //     {
+            //         Console.WriteLine(e.Message);
+            //         Console.WriteLine("Month must be between 1 and 12. Please try again.");
+            //     }
+            //     catch(FormatException)
+            //     {
+            //         Console.WriteLine("Invalid input. Please try again.");
+            //     }
+            //     hasRightFormat = true;
+            // }while(!hasRightFormat);
+
+            // Console.WriteLine("Enter a month:");
+            // month = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Enter a day:");
+            // day = int.Parse(Console.ReadLine());
+      //validate whether the date was created correctly and catch an error if not
+      //catch any format errors as well
+      Console.Read();
         }
     }
 }
